fc701937a046aecd50c46a73170632111278dd02
Akinyemi <karrim4real@gmail.com>
2020-03-29 13:17:24 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Philuxy.com || Best  Online Making Money News</title>
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/animate.css">
+		<link rel="stylesheet" href="css/fork-awesome.css">
+		<link rel="stylesheet" href="css/custom.css">
+		<link rel="stylesheet" href="css/style.css">
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/bootstrap.js"></script>
+
+
+
+	</head>
+		<body>
+
+			<!-- Nav Bar -->
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+  	<div class="container-fluid">
+   	 <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <!-- <img src="assest/img/logo.jpg" alt="logo" class="img-logo img-rounded"> -->
+      <a class="navbar-brand" href="index.html" >PHILUXY BLOG</a>
+   	  </div>
+	    <div class="collapse navbar-collapse" id="myNavbar" >
+	      <ul class="nav navbar-nav">
+		        <li class="active">
+		        	<a href="index.html"><span class="fa fa-book"></span> Home</a>
+		        </li>
+		        
+		        <li>
+		        	<a href="#"> <span class="fa fa-contact">Forum</a></span>
+		        </li> 
+		        <li>
+		        	<a href="#">How Its Works</a>
+		        </li> 
+		      </ul>
+			      <ul class="nav navbar-nav navbar-right">
+				       <li class="hidden-xs">
+				        	<a href="#"><span class="fa fa-about"> 15,450 &nbsp;  Members &nbsp;</span></a>
+				        </li>
+				        <li>
+				        	<a href="#"><span class="fa fa-group">&nbsp; Login</a></span>
+				        </li> 
+				        <li>
+				        	<a href="#"> <span class="fa fa-user"></span> Register</a>
+				        </li>
+		        <!-- <li><a href="#"><span class="fa fa-login"></span> Login</a></li> -->
+		      </ul>
+    </div>
+  </div>
+	</nav>
+	<!-- End of Navigation  -->
+
+
+	<!-- Berging of Couseral Slider -->
+
+	<div class="container-fluid">
+		
+		<!-- <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+	  <div class="carousel-inner">
+	    <div class="carousel-item active">
+	      <img src="img/alabiahub.jpg" class="d-block w-100" alt="...">
+	    </div>
+	    <div class="carousel-item">
+	      <img src="img/alabiahub.jpg" class="d-block w-100" alt="...">
+	    </div>
+	    <div class="carousel-item">
+	      <img src="img/alabiahub.jpg" class="d-block w-100" alt="...">
+	    </div>
+	  </div>
+	  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+</div> -->
+
+
+	<section class="main">
+		<div class="col-md-8">
+			<div class="content">
+				<h1>This is Main Page</h1>
+			</div>
+		</div>
+
+		<div class="col-md-4">
+			<h1>This is Side Bar</h1>
+		</div>
+	</section>
+
+
+	</div>
+	
+
+		</body>
+
+</html>
